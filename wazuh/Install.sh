44eb605ef0bebc7dbd741eb37385723bd07e62e5
Shashi Raj <98850848+EmperialX@users.noreply.github.com>
2024-05-16 02:33:47 +0530

    Update Install.sh

--- wazuh/Install.sh
+++ wazuh/Install.sh
@@ -1,41 +1,28 @@
 #!/bin/bash
 
-PROJECT_DIR="wazuh-docker"
-GIT_URL="https://github.com/wazuh/wazuh-docker.git"
-BRANCH="v4.7.4"
-if [ ! -d "$PROJECT_DIR" ]; then
-    echo "Cloning the Wazuh Docker project..."
-    git clone "$GIT_URL" -b "$BRANCH" && \
-    echo "Cloning successful." || \
-    { echo "Cloning failed. Exiting."; exit 1; }
-else
-    echo "Project directory already exists. Skipping clone."
-fi
-echo "Removing unwanted files and folders..."
-rm -rf "$PROJECT_DIR/build-docker-images/" \
-       "$PROJECT_DIR/CHANGELOG.md" \
-       "$PROJECT_DIR/indexer-certs-creator/" \
-       "$PROJECT_DIR/LICENSE" \
-       "$PROJECT_DIR/multi-node/" \
-       "$PROJECT_DIR/README.md" \
-       "$PROJECT_DIR/VERSION" && \
-echo "Cleanup successful." || \
-{ echo "Cleanup failed. Exiting."; exit 1; }
-cd "$PROJECT_DIR/single-node" && \
-echo "Generating indexer certificates..."
-docker-compose -f generate-indexer-certs.yml run --rm generator && \
-echo "Certificate generation successful." || \
-{ echo "Certificate generation failed. Exiting."; exit 1; }
-echo "Modifying Docker Compose file..."
-sed -i 's/9200:9200/9203:9200/g' docker-compose.yml && \
-sed -i 's/443:5601/5500:5601/g' docker-compose.yml && \
-echo "Docker Compose file modified successfully." || \
-{ echo "Failed to modify Docker Compose file. Exiting."; exit 1; }
-echo "Setting vm.max_map_count parameter..."
-sysctl -w vm.max_map_count=262144 && \
-echo "vm.max_map_count parameter set successfully." || \
-{ echo "Failed to set vm.max_map_count parameter. Exiting."; exit 1; }
-echo "Starting Docker Compose services..."
-docker-compose up -d && \
-echo "Docker Compose services started successfully." || \
-{ echo "Failed to start Docker Compose services. Exiting."; exit 1; }
+# Clone the Wazuh Docker project
+git clone https://github.com/wazuh/wazuh-docker.git -b v4.7.4
+
+# Navigate to the single-node directory
+cd wazuh-docker/single-node &&
+
+# Check if the configuration is already present
+if ! grep -q "<remote>\n    <connection>syslog<\/connection>\n    <port>5140<\/port>\n    <protocol>udp<\/protocol>\n    <allowed-ips>0.0.0.0\/0<\/allowed-ips>\n  <\/remote>" config/wazuh_cluster/wazuh_manager.conf; then
+    # If not, append it
+    echo "Appending configuration to Wazuh manager configuration file..." &&
+    echo -e "\n<remote>\n    <connection>syslog</connection>\n    <port>5140</port>\n    <protocol>udp</protocol>\n    <allowed-ips>0.0.0.0/0</allowed-ips>\n  </remote>" >> config/wazuh_cluster/wazuh_manager.conf &&
+    echo "Configuration appended successfully."
+fi &&
+
+# Generate the certificates
+docker-compose -f generate-indexer-certs.yml run --rm generator &&
+
+# Modify Docker Compose file
+sed -i 's/9200:9200/9201:9200/g' docker-compose.yml &&
+sed -i 's/443:5601/5500:5601/g' docker-compose.yml &&
+
+# Set the vm.max_map_count parameter
+sysctl -w vm.max_map_count=262144 &&
+
+# Start the Docker Compose services
+docker-compose up -d
